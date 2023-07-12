6daa03f35521a2d21f10beba467cf07ee4948e11
Marcelo A F Gomes <mafgomesinfo@gmail.com>
2023-07-12 01:23:26 -0300

    Removi prints; fixadas versões de dependências; removi dependências inúteis

--- lib/models/poke_api_types.dart
+++ lib/models/poke_api_types.dart
@@ -1,19 +1,18 @@
 import 'package:pokedex/models/models.dart';
 
 class PokeApiTipo {
-	var numTipos = 0;
+	static var id = 0;
 	List<Tipo> tipos;
 
 	PokeApiTipo({required this.tipos}) {
-		numTipos = tipos.length;
+		id += tipos.length;
 	}
 
 	factory PokeApiTipo.fromJson(List<dynamic> jsonData) {
 		var tipos = <Tipo>[];
-		var id = 0;
 		jsonData.forEach((e) {
-			id = id + 1;
 			tipos.add(Tipo(id: id, name: e["name"], url: e["url"]));
+			id = id + 1;
 		});
 		return PokeApiTipo(tipos: tipos);
 	}
